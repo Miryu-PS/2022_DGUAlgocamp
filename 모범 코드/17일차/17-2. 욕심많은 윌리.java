dbade48d3a2791e8419a632b71d27ece558ff078
Miryu-PS <ms990606@naver.com>
2022-08-09 08:22:02 +0900

    17일차 update

---
+++ 모범 코드/17일차/17-2. 욕심많은 윌리.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+
+		long[][] dp = new long[N + 1][W + 1];
+		int[] w = new int[N + 1];
+		int[] v = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dp[0][0] = 0L;
+		for(int i = 1; i <= W; i++) dp[0][i] = -1L;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j <= W; j++) {
+				dp[i][j] = dp[i-1][j];
+				if(j-w[i] >= 0) {
+					dp[i][j] = Math.max(dp[i][j], dp[i-1][j-w[i]]+v[i]);
+				}
+			}
+		}
+
+		Long ans = 0L;
+		for (int i = 0; i <= W; i++) {
+			ans = Math.max(dp[N][i], ans);
+		}
+		System.out.println(ans);
+	}
+}
